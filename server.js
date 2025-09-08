f207a484bb8665ce63192aeffd8adf488e58e5b1
Jay Tun <aungkyawtun.jay@gmail.com>
2025-09-08 23:52:00 +0200

    Intial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const authRoutes = require("./routes/auth");
+const characterRoutes = require("./routes/characters");
+const externalRoutes = require("./routes/external");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Rick and Morty API is running!");
+});
+
+app.use("/auth", authRoutes);
+app.use("/characters", characterRoutes);
+app.use("/external", externalRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
