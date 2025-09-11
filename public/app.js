77f174ccfb38aa6c97a054568f032763e107ed3e
Jay Tun <aungkyawtun.jay@gmail.com>
2025-09-12 01:22:24 +0200

    fix: for deployement and for localuse

--- public/app.js
+++ public/app.js
@@ -1,4 +1,7 @@
-const API_URL = "https://rick-and-morty-api-oh0e.onrender.com"; 
+const API_URL = window.location.hostname.includes("localhost")
+  ? "http://localhost:3000"
+  : "https://rick-and-morty-api-oh0e.onrender.com";
+ 
 
 document.getElementById("searchBtn")?.addEventListener("click", async () => {
     const name = document.getElementById("searchName").value.trim();
