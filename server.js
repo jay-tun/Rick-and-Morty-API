12a84cd2a89e5e9e3ed7f4332dc7b984a9361a1e
Jay Tun <aungkyawtun.jay@gmail.com>
2025-09-12 01:03:08 +0200

    fix: update api url for deployement

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const externalRoutes = require("./routes/external");
 const aiRoutes = require("./routes/ai");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://rick-and-morty-api-oh0e.onrender.com/",
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
